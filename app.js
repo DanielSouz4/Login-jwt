147c43085107ecfec19421cd52ec54349c9d0dc0
Daniel da silva souza <82838453+DanielSouz4@users.noreply.github.com>
2021-09-10 14:54:43 -0300

    Banco de dados

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRouter')
+const mongoose = require('mongoose')
 
 app.use('/user', userRouter)
+
+mongoose.connect(
+  process.env.MONGO_CONNERCTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  error => {
+    if (error) console.log(error)
+    else console.log('Mongo connected')
+  }
+)
 
 app.listen(process.env.PORT, () => {
   console.log('Server Running')
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/userController')
 
-router.post('/register', (req, res) => {
-  console.log('register')
-  res.send('register')
-})
+router.post('/register', userController.register)
 
-router.post('/login', (req, res) => {
-  console.log('login')
-  res.send('login')
-})
+router.post('/login', userController.login)
 
 module.exports = router
